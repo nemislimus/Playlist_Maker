a093a08c8b8f33bd735424be277d2004c18db429
nemislimus <nemislimus@yandex.ru>
2024-03-11 17:05:42 +0300

    Сделано ДЗ по 8 спринту

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainSearchButton = findViewById<Button>(R.id.SearchButton)
+        val mainMediatekaButton = findViewById<Button>(R.id.MediatekaButton)
+        val mainSettingsButton = findViewById<Button>(R.id.SettingsButton)
+
+        // Объявление анонимного объекта слушателя для кнопки из центральной части ДЗ (спринт 8)
+//        val mediatekaButtonClickListener: View.OnClickListener = object: View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity, "Совершаем переход в Медиатеку!", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+
+        mainSearchButton.setOnClickListener {
+            val clickOnSearchButton = Intent(this, SearchActivity::class.java)
+            startActivity(clickOnSearchButton)
+        }
+
+        mainMediatekaButton.setOnClickListener {
+            val clickOnMediatekaButton = Intent(this, MediatekaActivity::class.java)
+            startActivity(clickOnMediatekaButton)
+        }
+
+        mainSettingsButton.setOnClickListener {
+            val clickOnSettingsButton = Intent(this, SettingsActivity::class.java)
+            startActivity(clickOnSettingsButton)
+        }
+
+
     }
 }
